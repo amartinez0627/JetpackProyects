343a6f2c5137f04cf1f3932e82f4a44d214b8e19
Antono Martinez <antoniomartinez@elizondomx.com>
2020-10-11 23:18:12 -0500

    Implemented RecyclerView funct

--- RoomCrud/app/src/main/java/com/example/roomcrud/MainActivity.kt
+++ RoomCrud/app/src/main/java/com/example/roomcrud/MainActivity.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.roomcrud.databinding.ActivityMainBinding
 import com.example.roomcrud.db.SubscriberDAO
 import com.example.roomcrud.db.SubscriberDatabase
@@ -26,12 +27,18 @@ class MainActivity : AppCompatActivity() {
         binding.myViewModel = subscriberViewModel
         binding.lifecycleOwner = this
 
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView(){
+        binding.subscriberRecyclerView.layoutManager = LinearLayoutManager(this)
         displaySubscribersList()
     }
 
     private fun displaySubscribersList(){
         subscriberViewModel.subscribers.observe(this, Observer {
             Log.i("MYTAG", it.toString())
+            binding.subscriberRecyclerView.adapter = MyRecyclerViewAdapter(it)
         })
     }
 }
---
+++ RoomCrud/app/src/main/java/com/example/roomcrud/MyRecyclerViewAdapter.kt
@@ -0,0 +1,35 @@
+package com.example.roomcrud
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.recyclerview.widget.RecyclerView
+import com.example.roomcrud.databinding.ListItemBinding
+import com.example.roomcrud.db.Subscriber
+
+class MyRecyclerViewAdapter(private val subscribersList: List<Subscriber>): RecyclerView.Adapter<MyViewholder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewholder {
+        val  layoutInflater = LayoutInflater.from(parent.context)
+        val binding:ListItemBinding = DataBindingUtil.inflate(layoutInflater,R.layout.list_item,parent,false)
+
+        return  MyViewholder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return subscribersList.size
+    }
+
+    override fun onBindViewHolder(holder: MyViewholder, position: Int) {
+        holder.bind(subscribersList[position])
+    }
+
+}
+
+class MyViewholder(val binding:ListItemBinding): RecyclerView.ViewHolder(binding.root){
+
+    fun bind(subscriber: Subscriber){
+        binding.nameTextView.text = subscriber.name
+        binding.emailTextView.text = subscriber.email
+    }
+}
