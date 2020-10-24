d0f8b3fc117062cd427687dcc9ef2c891e861828
Antono Martinez <antoniomartinez@elizondomx.com>
2020-10-24 13:21:37 -0500

    log interceptor and made code cleaner

--- RetrofitDemo/app/src/main/java/com/example/retrofitdemo/MainActivity.kt
+++ RetrofitDemo/app/src/main/java/com/example/retrofitdemo/MainActivity.kt
@@ -11,18 +11,45 @@ import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+    lateinit var retService: AlbumService
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val retService = RetrofitInstance
+        retService = RetrofitInstance
             .getRetrofitInstance()
             .create(AlbumService::class.java)
 
+        getRequestWithQueryParameters()
+        //getRequestWithPathParameters()
+
+
+    }
+
+    private fun getRequestWithQueryParameters(){
         val responseLiveData:LiveData<Response<Albums>> = liveData {
             val response = retService.getSortedAlbums(3)
             emit(response)
         }
+
+        responseLiveData.observe(this, Observer {
+            val albumsList = it.body()?.listIterator()
+            if (albumsList != null){
+                while (albumsList.hasNext()){
+                    val albumsItem = albumsList.next()
+                    Log.i("MYTAG",albumsItem.title)
+                    val result = " "+"Album Title : ${albumsItem.title}"+"\n"+
+                            " "+"Album Id : ${albumsItem.id}"+"\n"+
+                            " "+"User Id : ${albumsItem.userId}"+"\n\n\n"
+
+                    text_view.append(result)
+
+                }
+            }
+        })
+    }
+
+    private fun getRequestWithPathParameters(){
 
         val pathResponse:LiveData<Response<AlbumsItem>> = liveData {
             val response = retService.getAlbum(3)
@@ -32,22 +59,6 @@ class MainActivity : AppCompatActivity() {
         pathResponse.observe(this, Observer {
             val title = it.body()?.title
             Toast.makeText(this,title,Toast.LENGTH_LONG).show()
-        })
-
-        responseLiveData.observe(this, Observer {
-            val albumsList = it.body()?.listIterator()
-            if (albumsList != null){
-                while (albumsList.hasNext()){
-                    val albumsItem = albumsList.next()
-                    Log.i("MYTAG",albumsItem.title)
-                    val result = " "+"Album Title : ${albumsItem.title}"+"\n"+
-                                 " "+"Album Id : ${albumsItem.id}"+"\n"+
-                                 " "+"User Id : ${albumsItem.userId}"+"\n\n\n"
-
-                    text_view.append(result)
-
-                }
-            }
         })
     }
 }
--- RetrofitDemo/app/src/main/java/com/example/retrofitdemo/RetrofitInstance.kt
+++ RetrofitDemo/app/src/main/java/com/example/retrofitdemo/RetrofitInstance.kt
@@ -1,15 +1,27 @@
 package com.example.retrofitdemo
 
 import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class RetrofitInstance {
+
+
     companion object{
-        val BASE_URL = "https://jsonplaceholder.typicode.com/"
+        private val interceptor = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+        private val client = OkHttpClient.Builder().apply {
+            addInterceptor(interceptor)
+        }.build()
+
+        private const val BASE_URL = "https://jsonplaceholder.typicode.com/"
         fun getRetrofitInstance():Retrofit{
             return  Retrofit.Builder()
                 .baseUrl(BASE_URL)
+                .client(client)
                 .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
                 .build()
         }
