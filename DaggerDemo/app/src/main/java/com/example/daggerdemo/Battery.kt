5e0b9d83d7fd1440a20e697eda5d62a16f2366b9
Antono Martinez <antoniomartinez@elizondomx.com>
2020-10-27 22:41:48 -0600

    Dagger Interface example

--- DaggerDemo/app/src/main/java/com/example/daggerdemo/Battery.kt
+++ DaggerDemo/app/src/main/java/com/example/daggerdemo/Battery.kt
@@ -3,11 +3,7 @@ package com.example.daggerdemo
 import android.util.Log
 import javax.inject.Inject
 
-class Battery @Inject constructor(){
-    init {
-        Log.i("MYTAG","Battery constructed")
-    }
-    fun getPower(){
-        Log.i("MYTAG","Battery power is available")
-    }
+interface Battery{
+
+    fun getPower()
 }
